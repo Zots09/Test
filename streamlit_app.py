5946f2cb2f9a47d659006ccefae9fe2d60f32093
Zots09 <98879240+Zots09@users.noreply.github.com>
2024-01-06 08:06:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+pip install streamlit
+import streamlit as st
 
+def main():
+    st.title("Hello Streamlit!")
+    st.write("Selamat datang di aplikasi Streamlit sederhana. Ini menampilkan tulisan 'Hello!'.")
+
+if __name__ == "__main__":
+    main()
+streamlit run app.py
